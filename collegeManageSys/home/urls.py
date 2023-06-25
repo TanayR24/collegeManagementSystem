021ca0444b770c6cfb05d3e8a8536c8171b75700
Tanay Raj <125906135+TanayR24@users.noreply.github.com>
2023-06-26 00:07:28 +0530

    Added the 2nd page for the web application

--- collegeManageSys/home/urls.py
+++ collegeManageSys/home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.home, name='home' )
+    path('', views.home, name='home' ),
+    path('ABC001', views.profile, name='ABC001')
 ]
 
